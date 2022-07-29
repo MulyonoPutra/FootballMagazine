7a477d670216999115069f96760e80420d140b74
Mulyono Putra <mulyonoputra1994@gmail.com>
2022-07-29 21:18:34 +0700

    Implements React-Hook-Form validations.

--- frontend-ts-version/src/pages/authentication/login/index.tsx
+++ frontend-ts-version/src/pages/authentication/login/index.tsx
@@ -1,31 +1,44 @@
-import React, { useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { Button, Input, Space } from '../../../components';
+import { Button, Space } from "../../../components";
+
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
+import { TextField } from "@mui/material";
 
 const styles = {
-  wrapper       : `min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8`,
-  wrapperWidth  : `max-w-md w-full space-y-8`,
-  loginText     : `mt-6 text-center text-3xl font-extrabold text-gray-900`,
-  formWrapper   : `rounded-md shadow-sm -space-y-px`,
-  imgSize       : `mx-auto h-12 w-auto`
-}
+  wrapper: `min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8`,
+  wrapperWidth: `max-w-md w-full space-y-8`,
+  loginText: `mt-6 text-center text-3xl font-extrabold text-gray-900`,
+  formWrapper: `rounded-md shadow-sm -space-y-px`,
+  imgSize: `mx-auto h-12 w-auto`,
+  invalid: `text-red-600 text-sm`
+};
 
-export interface ILoginProps {
+type ILoginProps = {
   email?: string;
   password?: string;
-}
+};
 
 const Login = () => {
 
-  const navigate = useNavigate();
-  const { search } = useLocation();
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().required("Email is required").email("Email is invalid"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(6, "Password must be at least 6 characters")
+      .max(40, "Password must not exceed 40 characters"),
+  });
 
-  const [login, setLogin] = useState<ILoginProps>({});
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ILoginProps>({
+    resolver: yupResolver(validationSchema),
+  });
 
-  const submitHandler = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log(`email: ${login.email}`);
-    console.log(`password: ${login.password}`);
+  const onSubmit = (data: ILoginProps) => {
+    console.log(JSON.stringify(data, null, 2));
   };
 
   return (
@@ -34,30 +47,36 @@ const Login = () => {
         <div>
           <img
             className={styles.imgSize}
-            src='https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg'
-            alt='Logo'
+            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+            alt="Logo"
           />
           <h2 className={styles.loginText}>Sign in to your account</h2>
         </div>
-        <form className='mt-8 space-y-6' onSubmit={submitHandler}>
-          <input type='hidden' name='remember' defaultValue='true' />
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit(onSubmit)}>
           <div className={styles.formWrapper}>
-            <Input
-              placeholder='Email Address'
-              handleChange={(e) => setLogin({...login, email: e.target.value})}
+            <TextField
+              type="text"
+              placeholder="Email Address"
+              {...register("email")}
+              className='w-full'
             />
+            <div className={styles.invalid}>{errors.email?.message}</div>
             <Space height={10} />
-            <Input
-              placeholder='Password'
-              handleChange={(e) => setLogin({...login, password: e.target.value})}
+            <TextField
+              placeholder="Password"
+              type="text"
+              {...register("password")}
+              className='w-full'
             />
+            <div className={styles.invalid}>{errors.password?.message}</div>
           </div>
           <div>
-            <Button title='Sign In'></Button>
+            <Button title="Sign In"></Button>
           </div>
         </form>
       </div>
     </div>
   );
 };
+
 export default Login;
